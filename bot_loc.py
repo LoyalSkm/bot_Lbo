d32b5ab89c1614fa3aef63f99bad8eb8a60d9943
LoyalSkm <konstienzp@gmail.com>
2019-05-25 17:17:51 +0300

    1

--- bot_loc.py
+++ bot_loc.py
@@ -4,7 +4,6 @@ import random
 from datetime import timedelta, datetime, date
 
 
-# print(telegram.__version__)
 from telegram.ext import Updater
 updater = Updater(token='834879381:AAH0BaqokKCa0znrmnywNN7kV8yN76JLQ5E',
                   use_context=True)
@@ -13,19 +12,41 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-
 def read_json():
     return json.load(open('users.json'))
 
 def write_json(person_dict):
     json.dump(person_dict, open('users.json', 'w'), ensure_ascii=False, indent=6)
 
+def mock(update, context):
+    users = read_json()
+    empt_list = [] #список учасников
+    hunted_user = [] #рользователь который словил +1
+    mock_list= ["ахаха ебать {} лох", "{} красный анус", "{} обоссан", "{} ебать ты лук", "{} барсук, садись скорей на сук",
+                "{} рак"]
+    time = str(date.today()) + " " + str(datetime.now().strftime("%H"))  # <<<<
+    try:
+        for key, value in users.items():  # поиск участников конкретной группы!!!!!< --- тут кастыль но я хз как по другому их искать
+            if str(update.message.chat_id) in key:  # поиск всех учасников по значению chat_id
+                empt_list.append(key)  # добавление всех зарегистрированных учасников
+        for i in empt_list:
+            if users[i]['data_time'] == time:
+                hunted_user.append(i)  # пользователь у которого стоит егоднешняя дата в JSON
+        name = users[hunted_user[0]]['first_name']
+        mock = random.choice(mock_list)
+        context.bot.send_message(chat_id=update.message.chat_id,
+                                 text=(mock.format(name)))
+    except IndexError:
+        context.bot.send_message(chat_id=update.message.chat_id,
+                                 text="У нас нет участников используй /rand")
 
 def reg(update, context):
     users = read_json()
     keys_all = users.keys()
-    keys_main = str(update.message.chat_id) + " " + str(update.message.from_user.id) # начало кастыля, чтобы конктеризировать когото пишлось делать это
-
+    if update.message.chat_id != update.message.from_user.id:
+        keys_main = str(update.message.chat_id) + " " + str(update.message.from_user.id) # начало кастыля, чтобы конктеризировать когото пишлось делать это
+    else:
+        keys_main = "self"
     if (keys_main) in keys_all:
         if users[keys_main]["registration"] == 1:
             context.bot.send_message(chat_id=update.message.chat_id,
@@ -47,7 +68,7 @@ def reg(update, context):
         }
         write_json(users)
         context.bot.send_message(chat_id=update.message.chat_id,
-                                 text=update.message.from_user.first_name + " " + "+")
+                                 text=update.message.from_user.first_name + " " + "в игре!")
 
 def unreg(update, context):
     users = read_json()
@@ -56,7 +77,7 @@ def unreg(update, context):
     write_json(users)
 
     context.bot.send_message(chat_id=update.message.chat_id,
-                             text=update.message.from_user.first_name + " " + "-")
+                             text=update.message.from_user.first_name + " " + "ну и пошел ты")
 
 def users(update, context):
     users = read_json()
@@ -65,30 +86,40 @@ def users(update, context):
         if str(update.message.chat_id) in key:
             if value['registration'] == 1:
                 context.bot.send_message(chat_id=update.message.chat_id,
-                                         text=value['first_name']+ " " + str(value['username'])+ " " + str(value['score']))
-def rand(update, context):
+                                         text=value['first_name']+ " " + str(value['username'])+ " быд в тренде: " + str(value['score']))
+
+
+
+def roll(update, context):
     users = read_json()
     empt_list = [] #список учасников
     hunted_user = [] #рользователь который словил +1
-    for key, value in users.items(): #поиск участников конкретной группы!!!!!< --- тут кастыль но я хз как по другому их искать
-        if str(update.message.chat_id) in key: #поиск всех учасников по значению chat_id
-            empt_list.append(key) #добавление всех зарегистрированных учасников
-    for i in empt_list:
-        if users[i]['data_time'] == str(date.today()):
-            hunted_user.append(i) #пользователь у которого стоит егоднешняя дата в JSON
-            break
-    if hunted_user == []:
-        rand = random.choice(empt_list)
+    time = str(date.today()) + " " + str(datetime.now().strftime("%H"))#<<<<
+    if update.message.chat_id == update.message.from_user.id:
         context.bot.send_message(chat_id=update.message.chat_id,
-                                 text=users[rand]['first_name'] + " " + "плюсанул +1")
-        users[rand]['score'] += 1
-        users[rand]['data_time'] = str(date.today())
-        print(empt_list)
-        write_json(users)
+                                 text="Ты не можешь это делать сам с собой")
     else:
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="сегодня уже был победитель" + " " + users[hunted_user[0]]['first_name']+ " " + str(users[hunted_user[0]]['username']))
+        try:
+            for key, value in users.items(): #поиск участников конкретной группы!!!!!< --- тут кастыль но я хз как по другому их искать
+                if str(update.message.chat_id) in key: #поиск всех учасников по значению chat_id
+                    empt_list.append(key) #добавление всех зарегистрированных учасников
+            for i in empt_list:
+                if users[i]['data_time'] == time:
+                    hunted_user.append(i) #пользователь у которого стоит егоднешняя дата в JSON
+            if hunted_user == []:
+                rand_choice = random.choice(empt_list)
+                context.bot.send_message(chat_id=update.message.chat_id,
+                                         text=users[rand_choice]['first_name']+ " " + users[rand_choice]['username']+ " " + "Prepare your anus")
+                users[rand_choice]['score'] += 1
+                users[rand_choice]['data_time'] = time
 
+                write_json(users)
+            else:
+                context.bot.send_message(chat_id=update.message.chat_id,
+                                         text="час унижения " + " " + users[hunted_user[0]]['first_name']+ " " + str(users[hunted_user[0]]['username']))
+        except IndexError:
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                     text="У нас нет участников")
 
 def cripta(update, context):
     context.bot.send_message(chat_id=update.message.chat_id,
@@ -97,8 +128,10 @@ def cripta(update, context):
                              text="ETH =" + " " + poloniex.get_btc("eth"))
 
 from telegram.ext import CommandHandler
-rand_handler = CommandHandler('rand', rand)
-dispatcher.add_handler(rand_handler)
+mock_handler = CommandHandler('mock', mock)
+dispatcher.add_handler(mock_handler)
+roll_handler = CommandHandler('roll', roll)
+dispatcher.add_handler(roll_handler)
 users_handler = CommandHandler('users', users)
 dispatcher.add_handler(users_handler)
 cripta_handler = CommandHandler('cripta', cripta)
